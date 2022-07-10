e7e280c3087790617f01ecce0a8654dc806e9214
zavvdev <igor.zaviriukha@onix-systems.com>
2022-07-10 14:42:00 +0300

    feat(typewriter): Added Typewriter component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import { Typewriter } from "components/Typewriter/Typewriter";
 import { namespaces } from "i18n/namespaces";
 import { MainLayout } from "layouts/MainLayout/MainLayout";
 import { useHomeStyles } from "pages/Home/Home.styles";
@@ -11,7 +12,9 @@ export function Home() {
     <MainLayout>
       <div className={classes.root}>
         <h1>{t("title")}</h1>
-        <p>{t("description")}</p>
+        <Typewriter>
+          <p>{t("description")}</p>
+        </Typewriter>
       </div>
     </MainLayout>
   );
