5432934f9f7f0356d65de07039f0ebc95b980d27
zavvdev <igor.zaviriukha@onix-systems.com>
2022-07-17 15:19:35 +0300

    develop-2

--- src/ui/layouts/MainLayout/MainLayout.styles.js
+++ src/ui/layouts/MainLayout/MainLayout.styles.js
@@ -3,7 +3,6 @@ import { createUseStyles } from "react-jss";
 const useMainLayoutStyles = createUseStyles((theme) => ({
   root: {
     padding: "0 5% 200px 5%",
-    border: "1px solid red",
     boxSizing: "border-box",
     minHeight: "100vh",
     backgroundColor: theme.backgroundColor,
