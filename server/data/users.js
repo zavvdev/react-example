b8e79cded75a55c74e902a209ec4e184d93abcdb
zavvdev <igor.zaviriukha@onix-systems.com>
2022-07-28 22:15:18 +0300

    feature(server): Added basic node js server

---
+++ server/data/users.js
@@ -0,0 +1,49 @@
+const users = [
+  {
+    id: 1,
+    name: "Andrew Once",
+    email: "andrewonce@gmail.com",
+    company: "Onix-Systems",
+    role: "Front-End Engineer",
+    level: "Senior",
+    icon: "👨‍💼",
+  },
+  {
+    id: 2,
+    name: "Oliver Moore",
+    email: "olivermoore@gmail.com",
+    company: "Epam Systems",
+    role: "Front-End Engineer",
+    level: "Middle",
+    icon: "🧑‍💼",
+  },
+  {
+    id: 3,
+    name: "Melissa Brown",
+    email: "melissabrown@yahoo.com",
+    company: "Netflix",
+    role: "QA Engineer",
+    level: "Junior",
+    icon: "👩‍💼",
+  },
+  {
+    id: 4,
+    name: "Christopher Williams",
+    email: "chriswilliams87@gmail.com",
+    company: "Twitter",
+    role: "Back-End Engineer",
+    level: "Senior",
+    icon: "👨🏽‍💼",
+  },
+  {
+    id: 5,
+    name: "Erica Smith",
+    email: "smitherica@yahoo.com",
+    company: "Facebook",
+    role: "Project Manager",
+    level: "Senior",
+    icon: "🦸🏽‍♀️",
+  },
+];
+
+module.exports = users;
