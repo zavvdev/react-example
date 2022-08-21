bf3ce4a29d266c7c1eb42f4e1360969509293ce6
zavvdev <igor.zaviriukha@onix-systems.com>
2022-08-21 19:28:03 +0300

    feat(saga): Added base saga cache system

--- src/core/store/api/user/middleware.js
+++ src/core/store/api/user/middleware.js
@@ -1,22 +1,36 @@
 import {
-  call, put, select, takeLatest,
+  all,
+  call,
+  put, takeLatest,
 } from "redux-saga/effects";
 import { USER_API_ACTION_TYPES } from "core/store/api/user/config";
 import { userApiActions } from "core/store/api/user/actions";
 import { http } from "core/api/http";
 import { HTTP_API_ENDPOINTS } from "core/config/http";
-import { selectAllUsers } from "core/store/api/user/selectors";
+import { request } from "core/store/api/middleware";
+// import { selectAllUsers } from "core/store/api/user/selectors";
 
-const getAll = async () => http.get(HTTP_API_ENDPOINTS.user.getAll());
+// const getAll = async () => http.get(HTTP_API_ENDPOINTS.user.getAll());
 
 function* getAllMiddlewareWorker() {
   try {
-    let users = yield select(selectAllUsers);
-    if (!users) {
-      const { payload } = yield call(getAll);
-      users = payload;
-    }
-    yield put(userApiActions.getAll.success({ data: users }));
+    yield all([
+      call(request, {
+        key: "getUsers",
+        fn: () => http.get(HTTP_API_ENDPOINTS.user.getAll()),
+      }),
+      call(request, {
+        key: "getUsers2",
+        fn: () => http.get(HTTP_API_ENDPOINTS.user.getAll()),
+      }),
+    ]);
+
+    // let users = yield select(selectAllUsers);
+    // if (!users) {
+    //   const { payload } = yield call(getAll);
+    //   users = payload;
+    // }
+    // yield put(userApiActions.getAll.success({ data: users }));
   } catch (e) {
     yield put(userApiActions.getAll.failure());
     throw e;
--- src/core/store/rootReducer.js
+++ src/core/store/rootReducer.js
@@ -3,10 +3,12 @@ import { themeReducer } from "core/store/theme/reducer";
 import { THEME_DOMAIN } from "core/store/theme/config";
 import { USER_API_DOMAIN } from "core/store/api/user/config";
 import { userApiReducer } from "core/store/api/user/reducer";
+import { apiReducer } from "core/store/api/reducer";
 
 const rootReducer = combineReducers({
   [USER_API_DOMAIN]: userApiReducer,
   [THEME_DOMAIN]: themeReducer,
+  api: apiReducer,
 });
 
 export { rootReducer };
