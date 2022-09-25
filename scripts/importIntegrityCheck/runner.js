5aba5ae10ead730dc51dbedf091bf5865f1c739e
zavvdev <igor.zaviriukha@onix-systems.com>
2022-09-25 17:56:24 +0300

    test

--- scripts/importIntegrityCheck/runner.js
+++ scripts/importIntegrityCheck/runner.js
@@ -2,6 +2,8 @@ const fs = require("node:fs");
 const { error, success } = require("../utils");
 const { checkFeatureImportsIntegrity } = require("./featureImportsIntegrity");
 
+let issues = [];
+
 try {
   const ROOT_DIR = "src";
   const DOMAIN = process.argv[2]?.toLowerCase();
@@ -10,24 +12,23 @@ try {
     throw new Error(`"${DOMAIN}" directory is not exists`);
   }
 
-  const issues = [
-    ...checkFeatureImportsIntegrity({
-      rootDir: ROOT_DIR,
-      domain: DOMAIN,
-    }),
-  ];
-
   if (issues.length > 0) {
-    error('Some modules in your project are not satisfying dependency rules!');
-    issues.forEach(issue => {
-      console.log('--------------------');
-      console.log(`Path: ${issue.filePath}`)
-      console.log(`Message: ${issue.message}.`)
-      console.log('--------------------');
-    });
+    issues = [
+      ...checkFeatureImportsIntegrity({
+        rootDir: ROOT_DIR,
+        domain: DOMAIN,
+      }),
+    ];
+    throw new Error();
   } else {
     success('No issues was found');
   }
-} catch (e) {
-  error(e.message);
+} catch {
+  error('Some modules in your project are not satisfying dependency rules!');
+  issues.forEach(issue => {
+    console.log('--------------------');
+    console.log(`Path: ${issue.filePath}`)
+    console.log(`Message: ${issue.message}.`)
+    console.log('--------------------');
+  });
 }
