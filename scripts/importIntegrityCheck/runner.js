f8365794f8fcde2bafe00d7a93606e0240846130
zavvdev <igor.zaviriukha@onix-systems.com>
2022-09-25 18:16:27 +0300

    develop: added import integrity check scripts

--- scripts/importIntegrityCheck/runner.js
+++ scripts/importIntegrityCheck/runner.js
@@ -2,35 +2,33 @@ const fs = require("node:fs");
 const { error, success } = require("../utils");
 const { checkFeatureImportsIntegrity } = require("./featureImportsIntegrity");
 
-let issues = [];
-
 try {
   const ROOT_DIR = "src";
   const DOMAIN = process.argv[2]?.toLowerCase();
 
   if (DOMAIN && !fs.existsSync(`${ROOT_DIR}/${DOMAIN}`)) {
-    throw new Error(`"${DOMAIN}" directory is not exists`);
-  }
-
-  issues = [
-    ...checkFeatureImportsIntegrity({
-      rootDir: ROOT_DIR,
-      domain: DOMAIN,
-    }),
-  ];
-
-  if (issues.length > 0) {
-    throw new Error('Integrity issues found.');
+    error(`"${DOMAIN}" directory is not exists`);
   } else {
-    success('No issues was found');
+    const issues = [
+      ...checkFeatureImportsIntegrity({
+        rootDir: ROOT_DIR,
+        domain: DOMAIN,
+      }),
+    ];
+
+    if (issues.length > 0) {
+      error('Some modules in your project are not satisfying dependency rules!');
+      issues.forEach(issue => {
+        console.log('--------------------');
+        console.log(`Path: ${issue.filePath}`);
+        console.log(`Message: ${issue.message}`);
+        console.log('--------------------');
+      });
+      throw new Error('Import integrity issues found. Check messages above');
+    } else {
+      success('No import integrity issues found');
+    }
   }
 } catch (e) {
-  error('Some modules in your project are not satisfying dependency rules!');
-  issues.forEach(issue => {
-    console.log('--------------------');
-    console.log(`Path: ${issue.filePath}`);
-    console.log(`Message: ${issue.message}`);
-    console.log('--------------------');
-  });
   throw new Error(e.message);
 }
