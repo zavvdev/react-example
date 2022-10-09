423f015a963e622721c8babf63a85371c084244f
zavvdev <igor.zaviriukha@onix-systems.com>
2022-10-09 22:09:28 +0300

    tests

--- src/app/containers/AppHeader/AppHeader.test.jsx
+++ src/app/containers/AppHeader/AppHeader.test.jsx
@@ -3,7 +3,7 @@ import { testUtils } from "app/tests/utils";
 import { AppHeader } from "app/containers/AppHeader/AppHeader";
 
 describe("AppHeader container", () => {
-  test("should render AppHeader", () => {
+  test("should render AppHeader (snapshot)", () => {
     const result = testUtils.render(<AppHeader />);
     expect(result.asFragment()).toMatchSnapshot();
   });
