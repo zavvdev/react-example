16ae644e7fa0bc584cff12d79674714e7d6f814c
zavvdev <igor.zaviriukha@onix-systems.com>
2022-10-21 21:43:25 +0300

    tests: added tests for books containers

---
+++ src/books/containers/BookItem/BookItem.test.jsx
@@ -0,0 +1,75 @@
+import { fireEvent, screen } from "@testing-library/react";
+import { testUtils } from "books/gateway/input";
+import { BookItem } from "books/containers/BookItem/BookItem";
+
+describe("BookItem container", () => {
+  test("should render BookItem which has isInCart=false", () => {
+    const result = testUtils.render(
+      <BookItem
+        title="Mock"
+        author="mock"
+        date="12/12/22"
+        price="$42"
+        cover="https://mock.com/42"
+        onAddToCart={() => {}}
+        onRemoveFromCart={() => {}}
+        isInCart={false}
+      />,
+    );
+    expect(result.asFragment()).toMatchSnapshot();
+  });
+
+  test("should render BookItem which has isInCart=true", () => {
+    const result = testUtils.render(
+      <BookItem
+        title="Mock"
+        author="mock"
+        date="12/12/22"
+        price="$42"
+        cover="https://mock.com/42"
+        onAddToCart={() => {}}
+        onRemoveFromCart={() => {}}
+        isInCart
+      />,
+    );
+    expect(result.asFragment()).toMatchSnapshot();
+  });
+
+  test("should call onAddToCart function", () => {
+    const onAddToCartMock = jest.fn();
+    testUtils.render(
+      <BookItem
+        title="Mock"
+        author="mock"
+        date="12/12/22"
+        price="$42"
+        cover="https://mock.com/42"
+        onAddToCart={onAddToCartMock}
+        onRemoveFromCart={() => {}}
+        isInCart={false}
+      />,
+    );
+    const addToCartButton = screen.getByText("bookItem.addToCart");
+    fireEvent.click(addToCartButton);
+    expect(onAddToCartMock).toBeCalled();
+  });
+
+  test("should call onRemoveFromCart function", () => {
+    const onRemoveFromCartMock = jest.fn();
+    testUtils.render(
+      <BookItem
+        title="Mock"
+        author="mock"
+        date="12/12/22"
+        price="$42"
+        cover="https://mock.com/42"
+        onAddToCart={() => {}}
+        onRemoveFromCart={onRemoveFromCartMock}
+        isInCart
+      />,
+    );
+    const removeFromCartButton = screen.getByText("bookItem.alreadyInCart");
+    fireEvent.click(removeFromCartButton);
+    expect(onRemoveFromCartMock).toBeCalled();
+  });
+});
--- src/books/gateway/input.js
+++ src/books/gateway/input.js
@@ -1,4 +1,7 @@
+export { httpApi } from "app/store/httpApi";
 export { Shared } from "app/shared";
 export { I18N_CONFIG } from "app/i18n/config";
-export { httpApi } from "app/store/httpApi";
 export { cartActions, cartSelectors } from "cart/gateway/output";
+
+// always in the end
+export { testUtils } from "app/tests/utils";
