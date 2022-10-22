ca628813873815bd4f040d528487c26cb27277d5
zavvdev <igor.zaviriukha@onix-systems.com>
2022-10-22 12:09:53 +0300

    tests: added response/request api adapters

--- src/books/store/api.js
+++ src/books/store/api.js
@@ -1,6 +1,16 @@
 import { httpApi } from "books/gateway/input";
 import { BOOKS_API_TAGS, BOOKS_HTTP_API_ENDPOINTS } from "books/store/config";
 
+const getAllBooksResponseAdapter = (response) =>
+  response.map((book) => ({
+    id: book?.id,
+    title: book?.title,
+    author: book?.author_fullname,
+    date: book?.publish_date,
+    price: book?.price,
+    cover: book?.cover_url,
+  }));
+
 const booksApi = httpApi.injectEndpoints({
   endpoints: (build) => ({
     getAllBooks: build.query({
@@ -8,6 +18,9 @@ const booksApi = httpApi.injectEndpoints({
         url: BOOKS_HTTP_API_ENDPOINTS.getAllBooks(),
       }),
       providesTags: [BOOKS_API_TAGS.getAllBooks],
+      transformResponse: (response) => {
+        return getAllBooksResponseAdapter(response);
+      },
     }),
   }),
 });
--- src/order/store/api.js
+++ src/order/store/api.js
@@ -1,16 +1,18 @@
 import { httpApi } from "order/gateway/input";
 import { ORDER_HTTP_API_ENDPOINTS } from "order/store/config";
 
+const postOrderRequestAdapter = ({ email, books }) => ({
+  order_email: email,
+  book_ids: books.map((book) => book.id),
+});
+
 const orderApi = httpApi.injectEndpoints({
   endpoints: (build) => ({
     postOrder: build.mutation({
       query: ({ email, books }) => ({
         url: ORDER_HTTP_API_ENDPOINTS.postOrder(),
         method: "POST",
-        data: {
-          email,
-          books,
-        },
+        data: postOrderRequestAdapter({ email, books }),
       }),
     }),
   }),
