6df11bdacc97067f4a991706ed25970ebc837aed
zavvdev <igor.zaviriukha@onix-systems.com>
2022-10-24 21:16:52 +0300

    tests

--- src/app/tests/mocks/apiServerMock.js
+++ src/app/tests/mocks/apiServerMock.js
@@ -2,4 +2,8 @@ import { setupServer } from "msw/node";
 
 const apiServerMock = setupServer();
 
+beforeAll(() => apiServerMock.listen());
+afterEach(() => apiServerMock.resetHandlers());
+afterAll(() => apiServerMock.close());
+
 export { apiServerMock };
