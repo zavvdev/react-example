bebb4d7da6acb16df3f4ec96849a5fd1d12e3b79
zavvdev <igor.zaviriukha@onix-systems.com>
2022-10-29 17:41:03 +0300

    tests: added tests for CartBook container

---
+++ src/cart/containers/CartBook/CartBook.test.jsx
@@ -0,0 +1,40 @@
+import { fireEvent, screen } from "@testing-library/react";
+import { CartBook } from "cart/containers/CartBook/CartBook";
+import { testUtils } from "__tests__/utils";
+
+const properties = {
+  cover: "https://mock.com/42",
+  title: "Mock title",
+  author: "Mock Author",
+  price: "$42",
+  onRemoveFromCart: jest.fn(),
+};
+
+const component = (
+  <CartBook
+    cover={properties.cover}
+    title={properties.title}
+    author={properties.author}
+    price={properties.price}
+    onRemoveFromCart={properties.onRemoveFromCart}
+  />
+);
+
+test("should render CartBook", () => {
+  const result = testUtils.render(component);
+  expect(result.asFragment()).toMatchSnapshot();
+});
+
+test("should have title, price & author", () => {
+  testUtils.render(component);
+  expect(screen.getByText(properties.title)).toBeInTheDocument();
+  expect(screen.getByText(properties.price)).toBeInTheDocument();
+  expect(screen.getByText(properties.author)).toBeInTheDocument();
+});
+
+test("should call onRemoveFromCart function", () => {
+  testUtils.render(component);
+  const removeButton = screen.getByText("cartBook.remove");
+  fireEvent.click(removeButton);
+  expect(properties.onRemoveFromCart).toBeCalled();
+});
