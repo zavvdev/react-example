d395338b65748ccc1b41a9f12e668c6bddbea6dd
zavvdev <igor.zaviriukha@onix-systems.com>
2022-11-03 20:12:16 +0200

    dev

--- src/app/http/index.js
+++ src/app/http/index.js
@@ -1,32 +1,15 @@
 import axios from "axios";
 import { HTTP_CONFIG } from "app/http/config";
-import { requestInterceptor, responseInterceptor } from "app/http/interceptors";
 import { createHttpService } from "app/services/HttpService";
 
-const createAxiosInstance = ({ endpoint, interceptors }) => {
-  const instance = axios.create({
+const createAxiosInstance = ({ endpoint }) => {
+  return axios.create({
     baseURL: endpoint,
   });
-
-  instance.interceptors.request.use(
-    interceptors.request.fulfilled,
-    interceptors.request.error,
-  );
-
-  instance.interceptors.response.use(
-    interceptors.response.fulfilled,
-    interceptors.response.error,
-  );
-
-  return instance;
 };
 
 export const http = createHttpService(
   createAxiosInstance({
     endpoint: HTTP_CONFIG.endpoint,
-    interceptors: {
-      response: responseInterceptor,
-      request: requestInterceptor,
-    },
   }),
 );
